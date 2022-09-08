5208d7fab3324e070f39cd78b4439775b8dafce0
KAROLIE <KAROLITENAI@GMAIL.COM>
2022-09-09 01:06:12 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 # include <stdio.h>
 /**
  *  main -entry point
- *  should print "Programming is like building a multilingual puzzle"
+ *  Description :should print "Programming is like building a multilingual puzzle"
  *  must return - 0 (success)
  *
  */
