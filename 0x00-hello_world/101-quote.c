7c333fc7b771ff26ef73739ccd57a3707279033c
KAROLIE <KAROLITENAI@GMAIL.COM>
2022-09-09 01:18:57 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 /**
  *   main: Entry point
- *   Description: writing c program thst prints
+ *   Description: writing c program that prints and that piece of art is useful
  *   Return: Always 1 (Success)
  */
 
 int main(void)
 {
- char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 write(2, str1, 59);
 
