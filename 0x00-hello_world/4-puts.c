eceac82e0ad248cd10fa1a38ebbb09c2a318ddcd
KAROLIE <KAROLITENAI@GMAIL.COM>
2022-09-09 01:10:20 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,9 @@
  *  main -entry point
  *  Description :should print "Programming is like building a multilingual puzzle"
  *  must return - 0 (success)
- *
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
