43e0ecfbf10d1a9a0aa52db038abf8c47c6b32bb
KAROLIE <KAROLITENAI@GMAIL.COM>
2022-09-09 00:04:48 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 /**
  *  main -entry point
  *
- *  must return 0 (success)
+ *  must return - 0 (success)
  *
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return 0;
+	return (0);
 }
